fdbc7ad2e923709c0ea941603a1e923af53827ba
aleahbarron-mcp <86987457+aleahbarron-mcp@users.noreply.github.com>
2021-07-05 16:59:37 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+}
+basic.forever(function () {
+	
+})
